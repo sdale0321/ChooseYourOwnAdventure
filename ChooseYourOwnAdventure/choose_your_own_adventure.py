0a169eaa679ae6d102170890484be6c550ed91e1
sdale0321 <67200326+sdale0321@users.noreply.github.com>
2024-04-04 22:17:46 -0500

    Added notes

--- ChooseYourOwnAdventure/choose_your_own_adventure.py
+++ ChooseYourOwnAdventure/choose_your_own_adventure.py
@@ -2,18 +2,19 @@ name = input("Type your name: ")
 print("Well hoinky doinky, welcome", name, "to this adventure!")
 
 answer = input("You are on a dirt road, it has come to an end and you can go left or right. Which way do you want to go?").lower()
-
+#by setting answer to input, it allows us to play this game of nesting loops 
 if answer == "left":
     answer = input("You come to a river, you can walk around or try to swim across.  Type walk to walk around or type swim to try to swim across: ").lower()
-    
-    if answer == "swim":
+    #by setting the condition for the users answer to match one of two words (for now), we can determine a "path" that the user wants to take.  
+    #Once that path is determined you can set the variable "answer" to yet another question to determine the next path.
+    if answer == "swim": #nesting the next set of if/elif/else statements within the first is what allows the users path to progress down multiple outcomes.  
         print("You swam across and were eaten by an alligator.")
     elif answer == "walk":
         print("You walked for many miles, ran out of water and you lost the game")
     else:
-        print("Not a valid optoin. You lose.")
+        print("Not a valid option. You lose.")
 
-elif answer == "right": 
+elif answer == "right": #part of the larger if/elif/else set of loops encapsulating the sub-paths a user can take.  
     answer = input("You come to a bridge, it looks wobbly. Do you want to cross it or head back (cross/back)? ")
 
     if answer == "back":
@@ -26,8 +27,9 @@ elif answer == "right":
         elif answer == "no":
             print("You ignore the stranger and they wait til you walk by to jump you.  You lose.")
         else:
-            print("Not a valid optoin. You lose.")
+            print("Not a valid option. You lose.")
     else:
-        print("Not a valid optoin. You lose.")
+        print("Not a valid option. You lose.") #this is a shorter example of this concept and can be added onto 
 else:
     print('Not a valid option. You lose.')
+print("Thank you for trying")
